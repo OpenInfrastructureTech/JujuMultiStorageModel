7987a4ca1995337b677ecfb4a164e8fce508c7c2
root <root@juju.openinfrastructure.tech>
2018-08-13 20:35:17 +0000

    baremetal VMs

---
+++ add-machines.sh
@@ -0,0 +1,11 @@
+juju add-machine ssh:ubuntu@192.168.5.12
+juju add-machine ssh:ubuntu@192.168.5.13 
+juju add-machine ssh:ubuntu@192.168.5.9
+juju add-machine ssh:ubuntu@192.168.5.8
+juju add-machine ssh:ubuntu@192.168.5.6
+juju add-machine ssh:ubuntu@192.168.5.3
+juju add-machine ssh:ubuntu@192.168.5.4
+juju add-machine ssh:ubuntu@192.168.5.16
+juju add-machine ssh:ubuntu@192.168.5.19
+juju add-machine ssh:ubuntu@192.168.5.15
+
